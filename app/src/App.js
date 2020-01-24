abc6eda85298342c4deba210c1f4cb78d17bda92
Mitali Karmarkar <mikarmar@microsoft.com>
2020-01-24 14:34:05 -0800

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -9,37 +9,6 @@ import {
   Switch
 } from 'react-router-dom'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Look at this really cool spinny thing we have here.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Everything is fine.
-//         </a>
-//         <a 
-//           className="App-link"
-//           href="https://authentication.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=serene-evergreen-1262eeae.mitalikarmarkar.net"
-//         >
-//         Log in with Facebook - Mitali change - see the build system works - 11/8 10:12am!
-//         </a>
-
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// const Home = () => (<div><h1>Welcome home</h1></div>)
 const Home = () => (<div><h1>Welcome MITALI</h1><Link to='/about'>Go to about</Link></div>)
 const About = ({ name }) => (<div><h1>About {name}</h1></div>)
 
