9f7622d38c89a98cb93579bd8d98c5dbd9308256
Mitali Karmarkar <mikarmar@microsoft.com>
2020-09-05 20:59:51 -0700

    push

--- app/src/App.js
+++ app/src/App.js
@@ -1,48 +1,26 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from './Home';
+import ShoppingList from './ShoppingList';
 
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Switch
 } from 'react-router-dom'
 
-const Home = () => (<div><h1>Welcome MITALI</h1><Link to='/about'>Go to about</Link></div>)
-const About = ({ name }) => (<div><h1>About {name}</h1></div>)
 
 class App extends React.Component {
   render() {
     return (
       <Router>
+      <div>
         <Switch>
-          <Route
-            path="/about"
-            render={(renderProps) => (
-              <div>
-                <Link to='/about/ari'>Ari</Link>
-                <Link to='/about/nate'>Nate</Link>
-                <Route
-                  path="/about/:name"
-                  render={(renderProps) => (
-                    <div>
-                      <About name={renderProps.match.params.name} />
-                      <Link to='/'>Go home</Link>
-                    </div>
-                  )} />
-              </div>
-            )} />
-          <Route
-            path="/"
-            render={(renderProps) => (
-              <div>
-                Last update on 8/10/20 11:32am
-                <Home {...this.props} {...renderProps} />
-                <a href="https://gray-sky-10e8573e.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=gray-sky-10e8573e.mitalikarmarkar.net/admin">Login</a>
-                <a href="https:/gray-sky-10e8573e.mitalikarmarkar.net/admin">Guarded link</a>
-              </div>
-            )} />
+          <Route path="/" component={Home} exact/>
+          <Route path="/shoppingList/" component={ShoppingList}/>
         </Switch>
+      </div>
       </Router>
     )
   }
