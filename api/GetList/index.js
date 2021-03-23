4a7450072cca1fe68262b796dfcb5e284b0af326
mkarmark <mikarmar@microsoft.com>
2021-03-23 10:04:28 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,7 +2,18 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        headers: { 'foobar': 'baz'},
-        body: req.headers
+        headers: { 'foobar': 'baz', 'bb':'cc'},
+    // status: 200, /* Defaults to 200 */
+    body: 'Hello, world.',
+        cookies: [
+      {
+        name: 'cookie1',
+        value: 'value1'
+      },
+      {
+        name: 'cookie2',
+        value: 'value2'
+      }
+    ]
     }
 };
