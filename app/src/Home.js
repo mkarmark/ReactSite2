1946952d8d7b9255b8540aec9888faed23f181e1
mkarmark <mikarmar@microsoft.com>
2021-04-07 12:06:50 -0700

    Update Home.js

--- app/src/Home.js
+++ app/src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my site! Last updated 4/7/21 12:01pm
+          Welcome to my site! Last updated 4/7/21 12:06pm
         </p>
         <a
           className="App-link"
